e80a51c78a9d1679d3ded2f9bc17b3eab22de551
amrlsymlx <amirulsyamil.ahmad@gmail.com>
2022-09-09 15:55:28 +0800

    Update class_07.js

--- class_07.js
+++ class_07.js
@@ -18,3 +18,32 @@
 
 //     }
 // }
+
+
+// class Student{
+//     constructor(name,age){
+//         this.name=name;
+//         this.age=age;
+//     }
+// }
+
+// we can use classes to create objects;
+const student_1 = new Student ('sara',22);
+const student_2 = new Student ('kim',25);
+
+// console.log(student_2,student_1);
+
+class Student{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+    dob(){
+        let date = new Date();
+        return date.getFullYear() - this.age;
+    }
+}
+
+console.log
+
+// review
