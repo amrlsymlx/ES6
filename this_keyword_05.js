f6b94a1c03fdcf5009da9f386799f6b7bd1e447c
amrlsymlx <amirulsyamil.ahmad@gmail.com>
2022-09-22 13:00:53 +0800

    Update this_keyword_05.js

--- this_keyword_05.js
+++ this_keyword_05.js
@@ -1,15 +1,14 @@
-// in a method, this refres to the owner object
-// alone, this refrers to the global object (window)
-// in a fucntion, it refers to the global object
-// in a fucntion, in strict mode its undefined
-// in an event , this refers to the element the received this event
-//methods like call() and apply() can refer to any object
-
+//In a method , this refers to the owner object
+// Alone , this refers to the global object (window)
+//In a function , it refers to the global object
+//In a function , in strict mode its undefined
+//In an event , this refers to the element that received the event
+//methods like call() and apply() can refer this to any object
 
 const user_1 = {
     firstName : 'sara',
-    lastName: 'smith',
-    id:100,
+    lastName:'smith',
+    id: 100,
     fullName: function(){
         return this.firstName+ " "+this.lastName;
     }
@@ -17,52 +16,53 @@ const user_1 = {
 
 const user_2 = {
     firstName : 'kim',
-    lastName: 'smith',
-    id:100,
+    lastName:'smith',
+    id: 102,
     fullName: function(){
         return this;
     }
 };
 
-
-
+//In an object method , this refers to the "owner" of the method
+// The user object is the owner of the method
 console.log(user_1);
 console.log(user_1.fullName());
 
 console.log(user_2);
 console.log(user_2.fullName());
 
-// this alone
-// in a browser the global object is window
+//This alone
+//In a browser the global object is window
 
 const x = this;
 console.log(x);
 
-// this is in functiom
-// in js function, the owner of the function
+//this  in the function
+//in js function, the owner of the function 
 // is the default binding for this
-// it means this refers to the global object
+//it means this refers to the global object
 
 function myFunction(){
     return this;
-};
+}
 
 console.log(myFunction());
 
-// function binding
-// the call() and apply() methods are predefined js methods
-// they can be both used to call an object method with another
+//Function binding 
+//the call() and apply() methods are predefined js methods
+//they can be both use to call an object method with another 
 // object as argument
 
-const customer= {
-    fullName:function (){
+const customer = {
+    fullName:function(){
         return this.firstName+ " "+this.lastName;
     }
 };
 
-const customer_1={
+const customer_1 = {
     firstName:'sara',
-    lastName:''
+    lastName:'smith',
 }
 
-// review this
+const test = customer.fullName.call(customer_1);
+console.log(test);
