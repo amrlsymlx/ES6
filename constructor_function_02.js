90c51b3ea6d7023683eb0b25ff808cd14c709b36
amrlsymlx <amirulsyamil.ahmad@gmail.com>
2022-09-22 12:55:18 +0800

    Update constructor_function_02.js

--- constructor_function_02.js
+++ constructor_function_02.js
@@ -1,28 +1,29 @@
-// Object Literals
+//object literal
 
 // const student = {
-//     firstName: 'Sara',
-//     lastName: 'Smith',
-//     age: '16',
+//     firstName: 'sara',
+//     lastName:'Smith',
+//     age:16
 // };
 
 // console.log(student.age);
 
-// Constructor function
+//constructor function
 // function Student(){
 //     this.firstName = 'Sara';
 //     this.lastName = 'Smith';
-//     this.age = 16;   
+//     this.age= 16;
 // };
 
 // const student_1 = new Student();
 // console.log(student_1);
 
-function Student(firstName, lastName, age){
+
+function Student(firstName,lastName,age){
     this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
+    this.lastName =lastName;
+    this.age= age;
 };
 
-const student_1 = new Student('Sara', 'Smith', 16);
+const student_1 = new Student('sara','smith',16);
 console.log(student_1);
