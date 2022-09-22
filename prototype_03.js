1f0a12bc50acc15f6ddb43ae4e57348eae89f268
amrlsymlx <amirulsyamil.ahmad@gmail.com>
2022-09-22 12:56:00 +0800

    Update prototype_03.js

--- prototype_03.js
+++ prototype_03.js
@@ -1,24 +1,24 @@
-// Object Literals
+//object literals
 
 // const user = {
-//     userName: 'Alex',
-//     age: 35
+//     userName:'alex',
+//     age:35
 // };
 
 // console.log(user);
 
-// Prototype: its an object which is associated with every object and
-// function in JS by default.
+//Prototype : its an object which is associated with every object and 
+//functions in js by default.
 
-// The prototype object is a special type of object with additional properties
-// and methods, which will be shared across all the object instances
-// of its constructor function
+//The prototype object is a special type of object with addtional propertis
+//and methods , which will be shared across all the object instances 
+//of its constructor function
 
-// Constructor function
+//constructor function 
 
-// function User() {
-//     this.name = 'Alex';
-//     this.age = 30;
+// function User (){
+//     this.name = 'alex';
+//     this.age= 30;
 // };
 
 // const user_1 = new User();
@@ -28,9 +28,9 @@
 // const user_2 = new User();
 // console.log(user_2);
 
-// Prototype
-function Student() {
-    this.name = 'Sara';
+//prototype
+function Student(){
+    this.name  = 'sara';
     this.age = 15;
 }
 
@@ -42,7 +42,7 @@ console.log(student_1);
 const student_2 = new Student();
 console.log(student_2);
 
-// How to caccess to the prototype
-console.log(student_1.prototype); // Undefined
-console.log(Student.prototype); // Object
-console.log(student_1.__proto__); // Object
+//How to access to the prototype 
+console.log(student_1.prototype); // undefined
+console.log(Student.prototype); //object
+console.log(student_1.__proto__); //object
