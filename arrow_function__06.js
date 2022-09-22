d1b0c524e695c1d7ebc1fc9da586d8db92c6f7d4
amrlsymlx <amirulsyamil.ahmad@gmail.com>
2022-09-22 13:13:42 +0800

    update

--- arrow_function__06.js
+++ arrow_function__06.js
@@ -1,22 +1,21 @@
 // with function expression
 
 // const greeting = function(){
-//     return 'Welcome to AAA'
+//     return 'Welcome to AAA';
 // }
 
 // console.log(greeting());
 
 // with arrow function
 // const greeting = () => {
-//     return 'Welcome to AAA'
-// }
+//     return 'Welcome to AAA';
+// };
 
 //it gets shorter if the function has only one statement
-//and the statement returns a value, so we can eliminate
+//and the statement returns a value , so we can eliminate
 // curly braces and return keyword as well
 
 // const greeting = () =>'Welcome to AAA';
-
 
 //Arrow function with parameters
 const greet = (user) => `Welcome to AAA ${user}`;
