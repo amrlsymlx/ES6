8303e5f980f74c6ddfc0090c4248fc7dfb51c8a9
amrlsymlx <amirulsyamil.ahmad@gmail.com>
2022-09-22 12:54:51 +0800

    Update block_scope_01.js

--- block_scope_01.js
+++ block_scope_01.js
@@ -1,34 +1,27 @@
-// global scope
+//global scope
 var a = 1;
 let b = 2;
 const c = 3;
 
-// function scope
+//function scope
 // function test(){
-//     var a = 4;
-//     let b = 5;
-//     const c=6
-
-//     console.log('Function :', a,b,c);
-// }
-
-// test();
-
-
-
-// block level scope
-// prob
-// if(true){
-
 //     var a = 4;
 //     let b = 5;
 //     const c = 6;
 
-//     console.log('Block Scope:'a,b,c);
+//     console.log('Function scope: ' , a,b,c);
 // }
 
+// test();
+
+//block level  scope
+if(true){
+    var a = 4;
+    let b = 5 ;
+    const c = 6;
+
+    console.log('Block level scope: ',a,b,c);
+}
 
 
-
-
-console.log('Global scope:',a,b,c);
+console.log('Global scope: ' , a,b,c);
