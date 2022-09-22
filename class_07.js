a9a1f50b7c0966a13a7c3cb8fd907b8b7704ae2a
amrlsymlx <amirulsyamil.ahmad@gmail.com>
2022-09-22 13:14:28 +0800

    Update class_07.js

--- class_07.js
+++ class_07.js
@@ -1,25 +1,21 @@
-// the constructor method is a special method,
-// it has the exact name as constructot
-// it is executed auto when new object is neign create
-// it is used to initialize the properties of an object
-// if we dont define the constructor function, js will 
-// an empty constructor function
+//the constructor method is a special method , 
+// it has the exact name as constructor
+//it is executed auto when new object is neign created
+//it is used to intialize the properties of an object 
+//if we dont defined the constructor function , js
+// will add an empty constructor function
 
-// js classes are the templates for js objects
-// a js class IS NOT AN OBJECT
-
-
+//js classes are the templates for js objects
+//a js class IS NOT AN OBJECT
 
 
-
-// syntax
+// syntax 
 // class ClassName{
 //     constructor(){
-
 //     }
 // }
 
-
+//This class has two initial properties
 // class Student{
 //     constructor(name,age){
 //         this.name=name;
@@ -27,9 +23,9 @@
 //     }
 // }
 
-// we can use classes to create objects;
-const student_1 = new Student ('sara',22);
-const student_2 = new Student ('kim',25);
+//we can use classes to create objects
+// const student_1 = new Student('sara',22);
+// const student_2 = new Student('kim',25);
 
 // console.log(student_2,student_1);
 
@@ -44,6 +40,5 @@ class Student{
     }
 }
 
-console.log
-
-// review
+let student_1 =new Student('sara',1990);
+document.body.innerHTML=`${student_1.name} is ${student_1.dob()}`;
