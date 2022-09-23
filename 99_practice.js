b6d3303fc1b28b1b6989cb5c374c16391d6ab277
amrlsymlx <amirulsyamil.ahmad@gmail.com>
2022-09-23 20:30:55 +0800

    Update 99_practice.js

--- 99_practice.js
+++ 99_practice.js
@@ -1,16 +1,28 @@
+// ------constructor function --------------------
+
 function User(email, name) {
     this.email = email;
     this.name = name;
     this.online = false;
-    this.login = function () {
-        console.log(this.email, 'has logged in');
-        
-    }
     
+}
+
+User.prototype.login = function () {
+    this.online = true
+    console.log(this.name, 'has logged in');
+}
+
+User.prototype.logout = function () {
+    this.online = false
+    console.log(this.name, 'has logged out');
 }
 
 let user_1 = new User('asd@gmail.com', 'Amir')
 let user_2 = new User('qwertyuiop@yahoo.com', 'Yoshi')
 
-console.log(user_1);
-user_2.login();
+// console.log(user_1);
+// user_2.login();
+
+/* yt link : https://www.youtube.com/watch?v=4jb4AYEyhRc */
+
+//--------------------------------------------------------------------
